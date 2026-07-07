28f2dfc5cf31038577da0fe0230298e13e26bf89
Benoit Vasseur <benoit.vasseur@zenika.com>
2026-07-07 11:59:03 +0200

    chore: purge Vue scaffold, minimal App/HomeView, reduce router to /
    Supprime HelloWorld, TheWelcome, WelcomeItem, les icônes du scaffold,
    AboutView et le test HelloWorld. Réécrit App.vue (RouterView seul),
    HomeView vide, et réduit le router à la route `/`. L'e2e vérifie
    désormais que #app est monté (toBeAttached) au lieu du texte "You did it!".
    
    Refs #16

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from './fixtures'
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
+test('visite la racine de l’application', async ({ page }) => {
   await page.goto('/')
-  await expect(page.locator('h1')).toHaveText('You did it!')
+  await expect(page.locator('#app')).toBeAttached()
 })
